c84a1888f79360f975f23fa5c86b8d14115d576b
Jorge Hernandez <jehernandezr@udistrital.edu.co>
2025-09-18 07:51:23 -0500

    first version of the baord - tic tac toe

--- 2. Tic Tac Toe/app/src/main/java/com/ud/tictactoeud/MainActivity.kt
+++ 2. Tic Tac Toe/app/src/main/java/com/ud/tictactoeud/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ud.tictactoeud.ui.theme.TicTacToeUdTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TicTacToeUdTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Game(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Game(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Player vs machine",
+        modifier = modifier
+    )
+    Column(
+
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        for (row in 0..2) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                for (column in 0..2) {
+                    Button(
+                        onClick = { /*TODO */ },
+                        modifier = Modifier
+                            .size(100.dp)
+                            .padding(4.dp)
+                    ) {
+                        Text(text = "(${row + 1}, ${column + 1})", color = Color.White)
+                    }
+                }
+            }
+        }
+    }
+
+    Text(
+        text = "Status: --",
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Game() {
     TicTacToeUdTheme {
-        Greeting("Android")
+        Game()
     }
 }
